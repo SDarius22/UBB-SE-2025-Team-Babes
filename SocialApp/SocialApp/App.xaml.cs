5e42afc3c0f832d57a45ab6e7b3530213ddfe01d
Sampalean Sorin <sampaleansorin@outlook.com>
2025-03-24 16:51:34 +0200

    Added the home screem and routed the OnLaunched to it

--- SocialApp/SocialApp/App.xaml.cs
+++ SocialApp/SocialApp/App.xaml.cs
@@ -42,8 +42,14 @@ namespace SocialApp
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+            Frame rootFrame = new Frame();
+            m_window.Content = rootFrame;
+            rootFrame.Navigate(typeof(HomeScreen), args.Arguments);
             m_window.Activate();
         }
+
+
+
 
         private Window? m_window;
     }
