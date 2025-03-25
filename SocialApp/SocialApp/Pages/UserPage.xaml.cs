8e68a42a164f946ae695f5d39b2f8fe9b2ca2d54
Silea Horia <horia.silea@gmail.com>
2025-03-25 13:13:45 +0200

    Clickclick

--- SocialApp/SocialApp/Pages/UserPage.xaml.cs
+++ SocialApp/SocialApp/Pages/UserPage.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.UI;
+using System.Drawing;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +25,79 @@ namespace SocialApp.Pages
     /// </summary>
     public sealed partial class UserPage : Page
     {
+
         public UserPage()
         {
             this.InitializeComponent();
+            SetContent();
+            SetPostsContent();
+        }
+
+        private void SetContent()
+        {
+            FollowLogOutButton.Content = IsLoggedIn() ? "Log Out" : (IsFollowed() ? "Unfollow" : "Follow");
+        }
+
+        private bool IsLoggedIn()
+        {
+            return false;
+        }
+
+        private bool IsFollowed()
+        {
+            return false;
+        }
+
+        private void PostsClick(object sender, RoutedEventArgs e)
+        {
+            SetPostsContent();
+        }
+
+        private void SetPostsContent()
+        {
+            PostsButton.IsEnabled = false;
+            WorkoutsButton.IsEnabled = true;
+            MealsButton.IsEnabled = true;
+            FollowersButton.IsEnabled = true;
+        }
+
+        private void WorkoutsClick(object sender, RoutedEventArgs e)
+        {
+            SetWorkoutsContent();
+        }
+
+        private void SetWorkoutsContent()
+        {
+            PostsButton.IsEnabled = true;
+            WorkoutsButton.IsEnabled = false;
+            MealsButton.IsEnabled = true;
+            FollowersButton.IsEnabled = true;
+        }
+
+        private void MealsClick(object sender, RoutedEventArgs e)
+        {
+            SetMealsContent();
+        }
+
+        private void SetMealsContent()
+        {
+            PostsButton.IsEnabled = true;
+            WorkoutsButton.IsEnabled = true;
+            MealsButton.IsEnabled = false;
+            FollowersButton.IsEnabled = true;
+        }
+
+        private void FollowersClick(object sender, RoutedEventArgs e)
+        {
+            SetFollowersContent();
+        }
+
+        private void SetFollowersContent()
+        {
+            PostsButton.IsEnabled = true;
+            WorkoutsButton.IsEnabled = true;
+            MealsButton.IsEnabled = true;
+            FollowersButton.IsEnabled = false;
         }
     }
 }
