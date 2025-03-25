8e214e0cd07932cfb46560237777a2314741305a
Silea Horia <horia.silea@gmail.com>
2025-03-25 15:58:13 +0200

    Added Home and User nav

--- SocialApp/SocialApp/Windows/HomeScreen.xaml.cs
+++ SocialApp/SocialApp/Windows/HomeScreen.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SocialApp.Pages;
 
 namespace SocialApp
 {
@@ -7,6 +9,23 @@ namespace SocialApp
         public HomeScreen()
         {
             this.InitializeComponent();
+            SetNavigation();
+        }
+
+        private void SetNavigation()
+        {
+            TopBar.HomeButtonInstance.Click += HomeClick;
+            TopBar.UserButtonInstance.Click += UserClick;
+        }
+
+        private void HomeClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(HomeScreen));
+        }
+
+        private void UserClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(UserPage));
         }
     }
 }
