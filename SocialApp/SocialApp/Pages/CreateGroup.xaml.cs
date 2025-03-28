d74993401d7b8a3341847ecef597efc100e8ff42
Silea Horia <horia.silea@gmail.com>
2025-03-28 13:09:57 +0200

    works

--- SocialApp/SocialApp/Pages/CreateGroup.xaml.cs
+++ SocialApp/SocialApp/Pages/CreateGroup.xaml.cs
@@ -10,12 +10,17 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using SocialApp.Entities;
 using SocialApp.Repository;
 using SocialApp.Services;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using static System.Net.Mime.MediaTypeNames;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,7 +35,7 @@ namespace SocialApp.Pages
         private AppController _controller;
         private GroupService _groupService;
         private UserService _userService;
-
+        private string image = string.Empty;
         public CreateGroup()
         {
             InitializeComponent();
@@ -78,7 +83,7 @@ namespace SocialApp.Pages
                     Name = GroupNameInput.Text.Trim(),
                     Description = string.IsNullOrWhiteSpace(GroupDescriptionInput.Text) ? null : GroupDescriptionInput.Text.Trim(),
                     AdminId = _controller.CurrentUser.Id,
-                    Image = null // Image handling can be implemented later
+                    Image = image
                 };
 
                 _groupService.ValidateAdd(newGroup.Name, newGroup.Description ?? "", newGroup.Image ?? "", newGroup.AdminId);
@@ -188,14 +193,37 @@ namespace SocialApp.Pages
             }
         }
 
-        private void AddImageButton_Click(object sender, RoutedEventArgs e)
+        private async void AddImageButton_Click(object sender, RoutedEventArgs e)
         {
-            // Add image logic here
+            try
+            {
+                var picker = new FileOpenPicker
+                {
+                    ViewMode = PickerViewMode.Thumbnail,
+                    SuggestedStartLocation = PickerLocationId.PicturesLibrary
+                };
+                picker.FileTypeFilter.Add(".jpg");
+                picker.FileTypeFilter.Add(".jpeg");
+                picker.FileTypeFilter.Add(".png");
+
+                var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.CurrentWindow);
+                WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+
+                StorageFile file = await picker.PickSingleFileAsync();
+                if (file != null)
+                {
+                    image = await AppController.EncodeImageToBase64Async(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorTextBox.Text = $"Error uploading image: {ex.Message}";
+            }
         }
 
         private void RemoveImageButton_Click(object sender, RoutedEventArgs e)
         {
-            // Remove image logic here
+            image = string.Empty;
         }
     }
 }
